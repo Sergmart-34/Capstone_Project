5803893fd04c2be1734e2a97efafe1d216dba4d5
Sergio Martin <sergiomartinalvarez34@gmail.com>
2025-12-11 16:16:10 +0100

    Delete immune_basic_agent directory

--- immune_basic_agent/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import agent  # ADK descubre el agente importando este módulo
-
-# Expone root_agent en el paquete para que ADK lo detecte con seguridad
-root_agent = agent.root_agent
-agent = agent.root_agent
-
-__all__ = ["root_agent", "agent"]
-
